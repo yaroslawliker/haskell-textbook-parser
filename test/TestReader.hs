83e245186a6f32c9c74e12ce3189c0d8032d2caf
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-11-25 20:42:06 +0200

    (test): add test of read text length

--- test/TestReader.hs
+++ test/TestReader.hs
@@ -1,14 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 module TestReader (readerTests) where
 
-import Data.Text (Text) -- Імпортуємо тип Text
+import qualified Data.Text as T
 import Test.HUnit
 import Reader (readBook)
 
 example_filepath :: String
 example_filepath = "test/res/ukr_example.txt" 
 
-expected_text :: Text
+expected_text :: T.Text
 expected_text = "Мова програмування Haskell названа на честь американського математика Гаскелла Каррі."
 
 testReader :: Test
@@ -16,7 +16,13 @@ testReader = TestCase $ do
     actual_text <- readBook example_filepath
     assertEqual "Should read book content correctly" expected_text actual_text
 
+testReaderTextLength :: Test
+testReaderTextLength = TestCase $ do
+    actual_text <- readBook example_filepath
+    assertEqual "Should return book length correctly" (T.length actual_text) 85
+
 readerTests :: Test
 readerTests = TestList [
-    TestLabel "Reader: readBook" testReader
+    TestLabel "Reader: readBook" testReader,
+    TestLabel "Reader: readBookLength" testReaderTextLength
     ]
