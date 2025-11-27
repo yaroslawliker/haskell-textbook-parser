522120d5254938547a7ec84262bd39a835c8b8ba
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-11-27 22:04:08 +0200

    (feat): add groupByFirstLetter

--- src/Arranger.hs
+++ src/Arranger.hs
@@ -1,7 +1,8 @@
 module Arranger where
 
 import qualified Data.Text as T
-import Data.List (sort)
+import Data.List (sort, groupBy)
+import Data.Function (on)
 import Parsing.Data ( Token(tokenType), Token(substring), TokenType(Word) )
 
 -- filterWords :: [Token] -> [Token]
@@ -15,3 +16,7 @@ import Parsing.Data ( Token(tokenType), Token(substring), TokenType(Word) )
 
 arrangeWords :: [Token] -> [T.Text]
 arrangeWords = sort . map substring . filter (\x -> tokenType x == Word)
+
+-- Works only for sorted
+groupByFirstLetter :: [T.Text] -> [[T.Text]]
+groupByFirstLetter = groupBy ( (==) `on` T.head )
