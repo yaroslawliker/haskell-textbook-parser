37652e2dfd52cadfec4c663ba6755572a6582b9a
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-11-27 21:42:43 +0200

    (feat + test): add arrangeWords and test

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,13 @@
 module Main where
 
 import System.IO (hSetEncoding, stdout, utf8)
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+
 import Reader (readBook)
 import Parsing.Parser (parseText)
 import Parsing.Data
-import Arranger
+import Arranger (arrangeWords)
 
 import Text.Pretty.Simple (pPrint)
 
@@ -14,11 +17,19 @@ printTokens (t:ts) = do
     pPrint t
     printTokens ts
 
+printWords :: [T.Text] -> IO ()
+printWords [w] = TIO.putStr w
+printWords (w:ws) = do
+    TIO.putStr $ w <>  (T.pack ", ")
+    printWords ws
+
 
 main :: IO ()
 main = do
-    let bookFile = "src/res/short_book.txt"
+    let bookFile = "src/res/book.txt"
     hSetEncoding stdout utf8
     book <- readBook bookFile
-    let tokens = filterWords $ parseText book
-    printTokens tokens
+    let tokens = parseText book
+    printWords $ arrangeWords tokens
+    TIO.putStrLn $ T.pack ""
+    -- printTokens tokens
--- src/Arranger.hs
+++ src/Arranger.hs
@@ -1,7 +1,17 @@
 module Arranger where
 
-import Parsing.Data ( Token(tokenType), TokenType(Word) )
+import qualified Data.Text as T
+import Data.List (sort)
+import Parsing.Data ( Token(tokenType), Token(substring), TokenType(Word) )
 
-filterWords :: [Token] -> [Token]
-filterWords = filter (\x -> tokenType x == Word)
+-- filterWords :: [Token] -> [Token]
+-- filterWords = filter (\x -> tokenType x == Word)
 
+-- extractWords :: [Token] -> [T.Text]
+-- extractWords = map substring
+
+-- sortWords :: [T.Text] -> [T.Text]
+-- sortWords = sort
+
+arrangeWords :: [Token] -> [T.Text]
+arrangeWords = sort . map substring . filter (\x -> tokenType x == Word)
