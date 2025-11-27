2c8e5f6abbf580c446e788d2682da8be49eebe37
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-11-27 22:04:45 +0200

    (feat + refact): move show methods into view and add printWordLists

--- app/Main.hs
+++ app/Main.hs
@@ -7,29 +7,15 @@ import qualified Data.Text.IO as TIO
 import Reader (readBook)
 import Parsing.Parser (parseText)
 import Parsing.Data
-import Arranger (arrangeWords)
-
-import Text.Pretty.Simple (pPrint)
-
-printTokens :: [Token] -> IO ()
-printTokens [] = return ()
-printTokens (t:ts) = do
-    pPrint t
-    printTokens ts
-
-printWords :: [T.Text] -> IO ()
-printWords [w] = TIO.putStr w
-printWords (w:ws) = do
-    TIO.putStr $ w <>  (T.pack ", ")
-    printWords ws
-
+import Arranger (arrangeWords, groupByFirstLetter)
+import View (printWordLists, printTokens)
 
 main :: IO ()
 main = do
-    let bookFile = "src/res/book.txt"
+    let bookFile = "src/res/short_book.txt"
     hSetEncoding stdout utf8
     book <- readBook bookFile
     let tokens = parseText book
-    printWords $ arrangeWords tokens
+    printWordLists $ groupByFirstLetter $ arrangeWords tokens
     TIO.putStrLn $ T.pack ""
-    -- printTokens tokens
+    
---
+++ src/View.hs
@@ -0,0 +1,30 @@
+module View where
+
+import Data.Text (Text, pack)
+import qualified Data.Text.IO as TIO
+import Text.Pretty.Simple (pPrint)
+
+import Parsing.Data (Token)
+
+-- Prints tokens, separate line for every
+printTokens :: [Token] -> IO ()
+printTokens [] = return ()
+printTokens (t:ts) = do
+    pPrint t
+    printTokens ts
+
+-- Print words with commas
+printWords :: [Text] -> IO ()
+printWords [w] = TIO.putStr w
+printWords (w:ws) = do
+    TIO.putStr $ w <> pack ", "
+    printWords ws
+
+-- Print words with commas and newline for each list
+printWordLists :: [[Text]] -> IO ()
+printWordLists [] = return ()
+printWordLists (l:ls) = do
+    printWords l
+    TIO.putStrLn $ pack ""
+    printWordLists ls
+
